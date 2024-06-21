2917fc4dd3a2b7f75dbd1464f3a38ea419258fb1
Vasilii <vasilii.zolotukhin@gmail.com>
2024-06-21 18:55:11 +1000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { CalendarList } from "react-native-calendars";
+
+export default function App() {
+  return <CalendarList current="2024-12-31" pagingEnabled />;
+}
